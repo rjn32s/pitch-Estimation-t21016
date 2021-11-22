1b05483c0431bf15dd71f8ebe9c252bfe9c8c8c8
Rajan Shukla T21016 <94681756+rjn32s@users.noreply.github.com>
2021-11-22 17:39:11 +0530

    Update resources.py
    Major changes in frequency_detect 
    ; added pitchEstimate

--- resources.py
+++ resources.py
@@ -7,52 +7,89 @@ Created on Thu Nov 18 12:22:42 2021
 
 import numpy as np
 import soundfile as sf
-import matplotlib.pyplot as plt
+from scipy import signal
 from math import log2, pow
+from scipy import fftpack
 
 #%%
 # Enframe Function
 def enframe(data , winsize , hoplen ,fs, wintype):
-  final_outcome = [] # For Storing the output
-  winsize = int(winsize* fs /1000) # Converting the ms to indexs
-  hoplen = int(hoplen* fs / 1000) 
-  for k in range(int(len(data)/hoplen)+2):
-    try:
-      zr = np.zeros(data.shape)
+    
+    final_outcome = []
+    winsize = int(winsize* fs /1000)
+    hoplen = int(hoplen* fs / 1000)
+    for k in range(int(len(data)/hoplen)+2):
+      try:
+        zr = np.zeros(data.shape)
         
-      if wintype =='rect':
-        zr[hoplen*k:k*hoplen+winsize] = np.ones(winsize)
-        result = zr * data
-      if wintype == 'hamm':
-        zr[hoplen*k:k*hoplen+winsize] = np.hamming(winsize)
-        result = zr * data
-    except: # Handling Error due to index overflow
-      result = zr
-    result = result[result !=0]
-    final_outcome.append(result)
-  return final_outcome
+        if wintype =='rect':
+          zr[hoplen*k:k*hoplen+winsize] = np.ones(winsize)
+          result = zr * data
+        if wintype == 'hamm':
+          zr[hoplen*k:k*hoplen+winsize] = np.hamming(winsize)
+          result = zr * data
+      except:
+        result = zr
+      result = result[result !=0]
+      final_outcome.append(result)
+    return final_outcome
 
 
 def frequency_detect(frame,fs):
+    
+    frame = frame - np.mean(frame)
+    rxx= signal.correlate(frame , frame , 'full', 'auto')
+    #peaks = signal.find_peaks(rxx)
+    #peaks, _ = signal.find_peaks(rxx,height=0)
+    #interval =np.diff(peaks)
+    #half_peak = peaks[len(peaks)//2:]
+    #interval =np.diff(half_peak)
+    #pitch= fs / interval[0]
+    
+    
+    corr = rxx[len(rxx) // 2 :]
+    dcorr = np.diff(corr)
+    rmin= np.where(dcorr > 0)[0]
+    #print(rmin)
+    if len(rmin) > 0:
+        rmin1 = rmin[0]
+    peak = np.argmax(corr[rmin1:]) + rmin1
+    #rmax = corr[peak] / corr[0]
+    fo = fs / peak
+    return fo
 
-  frame_len = int(len(frame))
-  rxx= np.correlate(frame , frame , 'full')
-  corr = rxx[len(rxx) // 2 :]
-  dcorr = np.diff(corr)
-  # Find the First minimum
-  rmin= np.where(dcorr > 0)[0]
-  if len(rmin) > 0:
-    rmin1 = rmin[0]
-  peak = np.argmax(corr[rmin1:]) + rmin1
-  rmax = corr[peak] / corr[0]
-  fo = fs / peak
-  return rmax , fo , peak
-
+#    rxx= signal.correlate(frame , frame , 'full', 'auto')
+#    corr = rxx[len(rxx) // 2 :]
+#    dcorr = np.diff(corr)
+#      # Find the First minimum
+#    rmin= np.where(dcorr > 0)[0]
+ #   if len(rmin) > 0:
+ #       rmin1 = rmin[0]
+ #   peak = np.argmax(corr[rmin1:]) + rmin1
+ #  rmax = corr[peak] / corr[0]
+  #  fo = fs / peak
+   # return rmax , fo , peak
+    
+#def capestrum_detect(frame , fs):
+    
+ #   X = np.abs(np.log10(fftpack.fft(frame)))
+        
+  #  corr = X[len(X) // 2 :]
+   # dcorr = np.diff(corr)
+        # Find the First minimum
+   # rmin= np.where(dcorr > 0)[0]
+   # if len(rmin) > 0:
+   #     rmin1 = rmin[0]
+   # peak = np.argmax(corr[rmin1:]) + rmin1
+   # rmax = corr[peak] / corr[0]
+   # fo = fs / peak
+    #return rmax , fo , peak
 
 
 
     
 def pitch(freq):
+    
     A4 = 440
     C0 = A4*pow(2, -4.75)
     name = ["C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B"]
@@ -60,4 +97,41 @@ def pitch(freq):
     octave = h // 12
     n = h % 12
     return name[n] + str(octave)
+#%%
 
+
+def pitchEstimate(address = " " , winSize = 30 , hopLEN = 15 , winType = 'hamm' ):
+    
+    
+    ########################################## Loading The .Wav File####################################
+    data, samplerate = sf.read(address) 
+    out = enframe(data, winsize=winSize , hoplen=hopLEN, fs = samplerate , wintype= winType) # Using Enframe from Resources
+    
+    
+    ########################################### Calculating the Energy In Each Frame For Segmentation ################
+    mask = []
+    for frame in out: 
+      #print('run')
+      
+      mask.append((np.abs(np.sum(frame * frame))))
+    avg_energy = np.mean(mask)
+    median_energy = np.median(mask)
+    ################################################################################################
+    
+    
+    
+    csv_out = []
+    for frame in out: 
+      if np.abs(np.sum(frame * frame)) >=avg_energy:
+          
+         # if app == 'acf':
+              
+          peak = frequency_detect(frame, samplerate)
+          csv_out.append(round(peak,3)) #, 'Voiced Frame',pitch(peak)])
+         # if app == 'freq':
+          #    rmax , fo , peak = capestrum_detect(frame, samplerate)
+           #   csv_out.append([round(fo,3) , 'Voiced Frame',pitch(fo)])
+              
+      else:
+          csv_out.append(0)
+    return csv_out
